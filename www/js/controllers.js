4ad36f523399a000fcb995224e0d845f501b5f66
Tony McCallie <tony@threeleaf.net>
2015-08-11 14:26:00 -0500

    square image

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -206,10 +206,8 @@ angular.module('greyback.controllers', [])
 		var options =   {
             quality: 50,
             destinationType: Camera.DestinationType.FILE_URI,
-			//allowEdit: true,
+			allowEdit: true,
 			correctOrientation: true,
-			targetWidth: 1170,
-			targetHeight: 640,
             sourceType: type,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
             encodingType: 0     // 0=JPG 1=PNG
         }
